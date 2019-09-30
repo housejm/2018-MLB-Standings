dcc5637e042d09d40e0e501dfacf093e25a13597
Matt House <j.matt.house@gmail.com>
2019-09-30 13:44:03 -0600

    Updated code comments for clarity

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Standings from './components/Standings'
 import * as serviceWorker from './serviceWorker';
 
+// Load the Standings component into the root element to render the web application
 ReactDOM.render(<Standings />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
